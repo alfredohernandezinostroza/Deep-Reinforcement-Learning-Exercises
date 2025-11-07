e2f33dacd29d2ae711bad4648bd5882f9570d6ff
Alfredo <alfredo.hernandez@ug.uchile.cl>
2025-11-07 11:23:31 +0000

    finish my implementation of pong learning

--- Pong/main.py
+++ Pong/main.py
@@ -1,6 +1,136 @@
 from wrappers import make_env
 import torch
-from typing import Tuple
+from typing import Tuple, List, Any, Deque
+import gymnasium as gym
+import numpy as np
+from dataclasses import dataclass
+from collections import deque
+import time
+from torch.utils.tensorboard import SummaryWriter
+
+DEVICE = 'cpu'
+DEFAULT_ENV_NAME = "PongNoFrameskip-v4" 
+TRAINING_BUFFER_SIZE = 10000
+GAMMA = 0.95
+INITIAL_EPSILON = 1.0
+EPSILON_DECAY_WINDOW = 0.01
+MINIMUM_EPSILON = 0.01
+EPSILON_DECAY = 0.999
+COPY_TO_TARGET_NETWORK = 1000
+MEAN_REWARD_BOUND = 19
+BATCH_SIZE = 32
+LEARNING_RATE = 0.9
+
+def main():
+    env = make_env(DEFAULT_ENV_NAME)
+    agent = Agent(env, buffer_size=TRAINING_BUFFER_SIZE)
+    epsilon = INITIAL_EPSILON
+    learning_net = deeepQNetwork(env.observation_space.shape, env.action_space.shape).to(DEVICE)
+    target_net   = deeepQNetwork(env.observation_space.shape, env.action_space.shape).to(DEVICE)
+    optimizer = torch.optim.Adam(learning_net.parameters, lr=LEARNING_RATE)
+    current_step = 1
+    ts_frame = 0
+    ts = time.time()
+    all_rewards = []
+    best_mean_reward = 0
+    writer = SummaryWriter(comment="-pong-dql")
+    while True:
+        done, reward = agent.step(epsilon, learning_net.detach())
+        if done:
+            all_rewards.append(reward)
+            speed = (current_step - ts_frame) / (time.time() - ts)
+            ts_frame = current_step
+            ts = time.time()
+            m_reward = np.mean(all_rewards[-100:])
+            print(f"{current_step}: done {len(all_rewards)} games, reward {m_reward:.3f}, "
+                  f"eps {epsilon:.2f}, speed {speed:.2f} f/s")
+            writer.add_scalar("epsilon", epsilon, current_step)
+            writer.add_scalar("speed", speed, current_step)
+            writer.add_scalar("reward_100", m_reward, current_step)
+            writer.add_scalar("reward", reward, current_step)
+            mean_reward = np.mean(all_rewards[-100:])
+            if mean_reward > best_mean_reward:
+                torch.save(learning_net, f"best_net_reward_{mean_reward:.0f}_iterations_{current_step}.dat")
+            if mean_reward > MEAN_REWARD_BOUND:
+                print("Solved in %d frames!" % current_step)
+                break
+        is_buffer_full = len(agent.buffer) == agent.buffer.maxlen
+        if not is_buffer_full:
+            continue
+        optimizer.zero_grad()
+        sample_from_buffer = np.random.choice(agent.buffer, BATCH_SIZE, replace=False)
+        loss = calculate_loss(sample_from_buffer, learning_net, target_net)
+        loss.backward()
+        optimizer.step()
+        if current_step % COPY_TO_TARGET_NETWORK == 0:
+            target_net.load_state_dict(learning_net.state_dict())
+        epsilon = max(MINIMUM_EPSILON, INITIAL_EPSILON - current_step/EPSILON_DECAY_WINDOW)
+        current_step += 1
+def calculate_loss(buffer: List, learning_net, target_net):
+    states_tensor, actions_tensor, next_states_tensor, rewards_tensor, dones_tensor, truncateds_tensor = get_tensors(buffer)
+    state_action_values: torch.Tensor = learning_net(states_tensor)
+    best_q_values = state_action_values.gather(dim=1,index=actions_tensor.unsqueeze(-1)).squeeze(-1)
+    with torch.no_grad:
+        next_q_values = target_net(next_states_tensor).max(1)[0]
+        next_q_values[dones_tensor] = 0
+        next_q_values.detach()
+    expected_state_action_values = rewards_tensor + GAMMA*next_q_values
+    mse_loss = torch.nn.MSELoss()
+    loss =  mse_loss(best_q_values, expected_state_action_values)
+    return loss
+
+def get_tensors(buffer: List) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor,]:
+    states, actions, next_states, rewards, dones, truncateds = [],[],[],[],[]
+    for step in buffer:
+        states.append(step.state)
+        actions.append(step.next_state)
+        next_states.append(step.next_state)
+        rewards.append(step.reward)
+        dones.append(step.done)
+        truncateds.append(step.truncated)
+    states = torch.as_tensor(np.asarray(states)).to(DEVICE)
+    actions = torch.LongTensor(np.asarray(next_states)).to(DEVICE)
+    next_states = torch.as_tensor(np.asarray(next_states)).to(DEVICE)
+    rewards = torch.FloatTensor(np.asarray(rewards)).to(DEVICE)
+    dones = torch.BoolTensor(np.asarray(dones)).to(DEVICE)
+    truncateds = torch.BoolTensor(np.asarray(truncateds)).to(DEVICE)
+    return states, actions, next_states, rewards, dones, truncateds
+
+@dataclass
+class Step:
+    state: np.ndarray
+    action: int
+    next_state: np.ndarray
+    reward: np.float32
+    dones: bool
+    truncated: bool
+    info: Any
+
+class Agent():
+    def __init__(self, env: gym.Env, buffer_size: int):
+        self.env = env
+        self.buffer: Deque[Step] = deque(maxlen=buffer_size)
+        self.state = self.env.reset()
+        self.total_reward = 0
+
+    def step(self, epsilon: float, learning_net_detached: torch.nn.Module) -> Tuple[bool, np.float32]:
+        if np.random.random() < epsilon:
+            action = self.env.action_space.sample()
+        else:
+            state_tensor = torch.as_tensor(self.state, device=DEVICE).unsqueeze_(0)
+            state_action_values = learning_net_detached(state_tensor)
+            _, action_tensor = torch.max(state_action_values, dim=1)
+            action = action_tensor.item()
+        new_state, reward, done, truncated, info = self.env.step(action)
+        step = Step(self.state, action, new_state, reward, done, truncated, info)   
+        self.buffer.append(step)
+        self.state = new_state
+        if done or truncated:
+            total_reward = self.total_reward
+            self.state = self.env.reset()
+            self.total_reward = 0
+            return True, total_reward
+        return False, None
 
 class deeepQNetwork(torch.nn.Module):
     def __init__(self, input_shape: Tuple[int, int, int], n_actions: int):
@@ -31,4 +161,4 @@ class deeepQNetwork(torch.nn.Module):
 
 
 if __name__ == "__main__":
-    enf = make_env("ALE/Pong-v5")
+    main()
