1cac3c472b242435b1362304bf05a6b21cd1f629
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2025-11-19 15:18:14 +0000

    delegate rendering to specific class

--- motor_learning/motor_environments.py
+++ motor_learning/motor_environments.py
@@ -1,12 +1,10 @@
 import gymnasium as gym
 from gymnasium.envs.registration import EnvSpec
 import numpy as np
-from typing import List, Tuple
-from typing import TypedDict
+from typing import List, Tuple, TypedDict, Dict
 import matplotlib.pyplot as plt
 import matplotlib
 from gymnasium.envs.registration import register
-
 register(
     id="Targets-v0",
     entry_point=f"{__name__}:Targets",
@@ -15,6 +13,66 @@ register(
 class Action(TypedDict):
     position: np.ndarray
     target: int
+
+class Renderer():
+    def __init__(self, initial_points: np.ndarray, render_mode: str):
+        self.render_mode = render_mode
+        if self.render_mode == "rgb_array":
+            matplotlib.use('Agg')
+        else:
+            matplotlib.use('TkAgg')            
+            plt.ion()  # Interactive mode
+        if self.render_mode == "human":
+            self.fig = plt.figure(figsize=(5, 5), dpi=90)
+        if self.render_mode == "rgb_array":
+            self.fig = plt.figure(figsize=(4.5, 4.5), dpi=200)
+        self.ax = self.fig.add_subplot(111)
+        self.ax.grid(True)
+        self.ax.set_aspect("equal", adjustable="box")
+        self.animated_artists = {}
+        # Draw targets as large colored dots
+        for i, pos in enumerate(initial_points):
+            self.ax.plot(pos[0], pos[1], 'o', markersize=20, label=f"Target {i}", color=plt.cm.tab10(i))
+
+    def set_limits(self, x_min, x_max, y_min=None, y_max=None):
+        self.ax.set_xlim(x_min, x_max)
+        assert (not y_min and not y_max) or (y_min and y_max), "y_min and y_max must be given!"
+        if y_min:
+            self.ax.set_ylim(y_min, y_max)
+        else:
+            self.ax.set_ylim(x_min, x_max)
+
+    def animate(self, animated_points: Dict):
+        if animated_points["position"].shape == (2,):
+            animated_points["position"] = np.expand_dims(animated_points["position"], axis=0)
+        if self.animated_artists.get(animated_points["artist_id"]):
+            self.animated_artists[animated_points["artist_id"]].set_offsets(animated_points["position"])
+            self.animated_artists[animated_points["artist_id"]].set_color('black')
+        else:
+            new_artist = self.ax.scatter(x=animated_points["position"][:,0],y=animated_points["position"][:,1], marker='o', color='black', s=30)
+            self.animated_artists[animated_points["artist_id"]] = new_artist
+
+    def render(self, point, color):
+        if color:
+            self.ax.plot(point[0], point[1], '.', color=plt.cm.tab10(color), markersize=8)
+
+        if self.render_mode == "human":
+            plt.pause(0.1)
+        
+        if self.render_mode == "rgb_array":
+            self.fig.canvas.draw()
+            image = np.frombuffer(self.fig.canvas.buffer_rgba(), dtype=np.uint8)
+            width, height = self.fig.canvas.get_width_height()
+            image = image.reshape((height, width, 4))  # Use actual dimensions
+            image = image[:, :, :3].copy()  # Drop alpha channel for RGB, and copy to avoid using the same memory adress for all frames
+            return image
+        
+    def close(self):
+        if self.fig is not None:
+            plt.close(self.fig)
+            self.fig = None
+            self.ax  = None
+            self.img = None
 
 class Targets(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 6}
@@ -28,19 +86,9 @@ class Targets(gym.Env):
                  training_area: Tuple,
                  max_trials: int,
                  *,
-                 render_mode=None 
+                 render_mode=None,
                  ):
         # super().__init__()
-        assert self.render_mode is None or self.render_mode in ["human", "rgb_array"]
-        self.render_mode = render_mode
-        if self.render_mode == "rgb_array":
-            matplotlib.use('Agg')
-        else:
-            matplotlib.use('TkAgg')            
-            plt.ion()  # Interactive mode
-        self.fig = None
-        self.ax = None
-        self.img = None
         self.n_targets = len(targets_positions)
         self.next_target_generator = lambda : np.random.randint(0,self.n_targets)
         self.action_space = gym.spaces.Dict(
@@ -56,7 +104,11 @@ class Targets(gym.Env):
         self.actions_history = []
         self.actions_history_by_target = [[]] * self.n_targets
         self.state = None
-        self.fig = None
+        assert self.render_mode is None or self.render_mode in ["human", "rgb_array"]
+        self.render_mode = render_mode
+        if render_mode:
+            self.renderer: Renderer = Renderer(initial_points=targets_positions, render_mode=render_mode)
+            self.renderer.set_limits(self.action_space["position"].low[0], self.action_space["position"].high[0])
 
     def reward_function(self, distance):
         """Could be edited to make more complicated reward functions"""
@@ -73,43 +125,16 @@ class Targets(gym.Env):
         distance_to_target = np.linalg.norm(action["position"] - self.targets_positions[action["target"]])
         reward = self.reward_function(distance_to_target)
         next_target = self.next_target_generator()
-        if self.render_mode == "human":
-            self.render()
+        # if self.render_mode == "human":
+        #     self.render()
         return next_target, reward, done, truncated, info
     
     def render(self):
-        if self.fig is None:
-            if self.render_mode == "human":
-                self.fig = plt.figure(figsize=(5, 5), dpi=90)
-            if self.render_mode == "rgb_array":
-                self.fig = plt.figure(figsize=(4.5, 4.5), dpi=200)
-            self.ax = self.fig.add_subplot(111)
-            self.ax.grid(True)
-            
-            # Set plotting limits for your training are
-            x_min = y_min = self.action_space["position"].low[0] 
-            x_max = y_max = self.action_space["position"].high[0]
-            self.ax.set_xlim(x_min, x_max)
-            self.ax.set_ylim(y_min, y_max)
-            self.ax.set_aspect("equal", adjustable="box")
-
-            # Draw targets as large colored dots
-            for i, pos in enumerate(self.targets_positions):
-                self.ax.plot(pos[0], pos[1], 'o', markersize=20, label=f"Target {i}", color=plt.cm.tab10(i))
+        point = color = None
         if len(self.actions_history) > 0:
-            last_action = np.array(self.actions_history[-1]["position"])
-            last_target = np.array(self.actions_history[-1]["target"])
-            self.ax.plot(last_action[0], last_action[1], '.', color=plt.cm.tab10(last_target), markersize=8)
-        if self.render_mode == "human":
-            plt.pause(0.1)
-        
-        if self.render_mode == "rgb_array":
-            self.fig.canvas.draw()
-            image = np.frombuffer(self.fig.canvas.buffer_rgba(), dtype=np.uint8)
-            width, height = self.fig.canvas.get_width_height()
-            image = image.reshape((height, width, 4))  # Use actual dimensions
-            image = image[:, :, :3].copy()  # Drop alpha channel for RGB, and copy to avoid using the same memory adress for all frames
-            return image
+            point = np.array(self.actions_history[-1]["position"])
+            color = np.array(self.actions_history[-1]["target"])
+        return self.renderer.render(point, color)
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
@@ -117,14 +142,10 @@ class Targets(gym.Env):
         self.state  = self.next_target_generator()
         next_target = self.next_target_generator()
         info = {}
-        if self.render_mode == "human":
-            self.render()
+        # if self.render_mode == "human":
+        #     self.render()
         return next_target, info
     
     def close(self):
-        if self.fig is not None:
-            plt.close(self.fig)
-            self.fig = None
-            self.ax  = None
-            self.img = None
+        self.renderer.close()
 
